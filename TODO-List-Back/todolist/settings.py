a2bf3e55c1f5548484ee5fc2de14dcfb03d0cd24
Semyon Berezovskiy <ber2semen@gmail.com>
2025-06-12 08:54:10 +0700

    Теперь без уязвимости

--- TODO-List-Back/todolist/settings.py
+++ TODO-List-Back/todolist/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#5&rtzgvc!#u$96!03^$8s65@tz7h*k7=*u^v*fjlk_nhr_kon'
-#SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-#5&rtzgvc!#u$96!03^$8s65@tz7h*k7=*u^v*fjlk_nhr_kon'
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
